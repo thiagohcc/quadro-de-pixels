42db63aad4376a60741dc0b7d6c3b492b8a6a604
thiagohcc <dzno23@gmail.com>
2023-02-08 23:26:13 -0300

    feat: configuracoes basicas iniciais

--- quadro-de-pixels/src/context/PixelProvider.js
+++ quadro-de-pixels/src/context/PixelProvider.js
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import PixelContext from "./PixelContext";
+import PropTypes from 'prop-types';
 
 function PixelProvider({ children }) {
   const [numberOfPixels, setNumberOfPixels] = useState();
   const [numberOfColors, setNumberOfColors] = useState();
   const [currentColor, setCurrentColor] = useState();
-  
+
+  const contextValue = useMemo(() => ({
+    numberOfPixels,
+    numberOfColors,
+    currentColor,
+    setNumberOfPixels,
+    setNumberOfColors,
+    setCurrentColor,
+  }), [numberOfPixels, numberOfColors, currentColor]);
 
   return (
-    <PixelContext.Provider>
+    <PixelContext.Provider value={ contextValue }>
       { children }
     </PixelContext.Provider>
   )
 }
+
+PixelProvider.propTypes = {
+  value: PropTypes.shape,
+}.isRequired;
 
 export default PixelProvider;
